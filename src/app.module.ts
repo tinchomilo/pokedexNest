ffa9be8b4fc68486633af61e0532117ea487da71
Martin <milone@fktech.net>
2023-09-12 15:23:58 -0300

    last changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -8,13 +9,18 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [ ServeStaticModule.forRoot({
+  imports: [ ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
     rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
   ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env);
+  }
+}
